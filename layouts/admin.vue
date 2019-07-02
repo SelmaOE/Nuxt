c37f328d93599599d18208d19bf559b93b83141b
sortegae <oeselma@yahoo.com>
2019-07-02 10:40:23 -0500

    upload up to section 6 - added axios module

---
+++ layouts/admin.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+
+    <nuxt />
+  </div>
+</template>
+
+<style>
+html {
+  font-family: 'Indie Flower', sans-serif;
+
+}
+body{
+  margin: 0;
+}
+
+</style>
---
+++ store/index.js
@@ -0,0 +1,67 @@
+import Vuex from "vuex";
+
+const createStore = () => {
+  return new Vuex.Store({
+    state: {
+      loadedPosts: []
+    },
+    mutations: {
+      setPosts(state, posts) {
+        state.loadedPosts = posts;
+      },
+      addPost(state, post){
+        state.loadedPosts.push(post)
+      },
+      editPost(state, editedPost){
+        const postIndex= state.loadedPosts.findIndex(
+          post => post.id=== editedPost.id);
+        state.loadedPosts[postIndex]= editedPost;
+      }
+    },
+    actions: {
+      nuxtServerInit(vuexContext, context) {
+        return context.app.$axios
+        .$get('/posts.json')
+          .then( data => {
+            const postArray=[]
+            for (const key in data){
+              postArray.push({...data[key], id: key})
+            }
+            vuexContext.commit('setPosts', postArray)
+          })
+          .catch(e => context.error(e));
+
+      },
+      setPosts(vuexContext, posts) {
+        vuexContext.commit("setPosts", posts);
+      },
+      addPost(vuexContext, post){
+        const createdPost= {
+          ...post,
+          updatedDate: new Date()
+        }
+
+        return this.$axios.$post('https://blog-tuto.firebaseio.com/posts.json',createdPost)
+        .then(data=> {
+          vuexContext.commit('addPost', {...createdPost, id: data.name})
+        })
+        .catch(e=> console.log(e));
+      },
+      editPost(vuexContext,editedPost){
+        return this.$axios.$put('https://blog-tuto.firebaseio.com/posts/'+
+        editedPost.id +'.json', editedPost)
+        .then(res => {
+          vuexContext.commit('editPost', editedPost)
+        })
+        .catch(e => console.log(e))
+      }
+    },
+    getters: {
+      loadedPosts(state) {
+        return state.loadedPosts;
+      }
+    }
+  });
+};
+
+export default createStore;
